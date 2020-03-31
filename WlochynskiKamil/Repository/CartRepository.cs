1a3e89f8fca9a6cfd95444a13ade8beaccab07b2
kwlochynski <44319598+kwlochynski@users.noreply.github.com>
2020-03-31 15:01:06 +0200

    Add files via upload

---
+++ WlochynskiKamil/Repository/CartRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WlochynskiKamil.Models;
+
+namespace WlochynskiKamil.Repository
+{
+    class CartRepository
+    {
+        public static List<Product> cartList = new List<Product>();
+
+       /* public void addProduct(Product product)
+        {
+            cartList.Add(product);
+        }*/
+    }
+}
---
+++ WlochynskiKamil/Repository/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using WlochynskiKamil.Models;
+
+namespace WlochynskiKamil.Repository
+{
+    class ProductRepository
+    {
+        string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "myDb2.db3");
+        //List<Product> products = new List<Product>();
+        public void addProduct(Product product)
+        {
+            var db = new SQLiteConnection(dbPath);
+
+
+
+            bool canAdd = true;
+            foreach(Product p in getAllProducts())
+            {
+                if(p.Name == product.Name)
+                {
+                    canAdd = false;
+                }
+            }
+           if (canAdd==true)
+            {
+                db.CreateTable<Product>();
+                db.Insert(product);
+            }
+
+        }
+
+        public List<Product> getAllProducts()
+        {
+            var db = new SQLiteConnection(dbPath);
+            List<Product> products = new List<Product>();
+            db.CreateTable<Product>();
+            products = db.Table<Product>().ToList();
+            return products;
+        }
+
+        public List<Product> getAllProductsByCategory(string category)
+        {
+            var db = new SQLiteConnection(dbPath);
+            List<Product> products = new List<Product>();
+            db.CreateTable<Product>();
+            products = db.Table<Product>().Where(r => r.Category == category).ToList();
+            return products;
+        }
+    }
+}
